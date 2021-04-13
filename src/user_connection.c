0fd25e2849b939a18d3ac8e04089a3958f267779
Antonin Fille <antonin.fille@epitech.eu>
2021-04-13 22:03:22 +0200

    user connexion with pass

--- src/user_connection.c
+++ src/user_connection.c
@@ -19,25 +19,34 @@ char *str_format(char *str)
 
 void user_connection(client_node_t *client, char *name)
 {
+    char msg[256] = "230 User logged in, proceed.\r\n";
+    char msg2[256] = "331 User name okay, need password.\r\n";
+
     client->name = str_format(name);
-    printf("%s\n", client->name);
+    write(client->fd, msg2, strlen(msg2));
+
+    (void)msg;
+}
+
+void  user_pass(client_node_t *client)
+{
+    char msg[256] = "230 User logged in, proceed.\r\n";
+    char msg2[256] = "332 Need account for login.\r\n";
+    if (!client->name) {
+        write(client->fd, msg2, strlen(msg2));
+        return;
+    }
+    client->pass = 1;
+    write(client->fd, msg, strlen(msg));
 }
 
 int user_connection_check(client_node_t *client, char **array)
 {
-    char msg[256] =  "530 Please login with USER and PASS.\r\n";
-    if (strcmp(array[0], "USER\r") == 0 && array[1]) {
+    if (strcmp(array[0], "USER") == 0 && array[1]) { 
         user_connection(client, array[1]);
+        return (0);
     } else if (strcmp(array[0], "PASS") == 0) {
-        
-    }
-    if (!client->name) {
-        write(client->fd, msg, strlen(msg));
-        return (-1);
-    }
-    if (!client->pass) {
-        write(client->fd, msg, strlen(msg));
-        return (-1);
+        user_pass(client);
     }
     return (0);
 }
