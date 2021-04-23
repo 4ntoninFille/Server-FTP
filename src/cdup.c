66cf41f2bec5a9bc17a8bdda1112b9437a8bc2b3
Antonin Fille <antonin.fille@epitech.eu>
2021-04-23 15:19:52 +0200

    add of return code in CDUP

--- src/cdup.c
+++ src/cdup.c
@@ -11,7 +11,10 @@ void cdup_command(client_node_t *client)
 {
     int lock = 1;
     char *new = NULL;
-    if (strlen(client->pwd) == 1) return;
+    if (strlen(client->pwd) == 1) {
+        dprintf(client->fd, "504 Command not implemented for that parameter.\r\n");
+        return;
+    }
     new = malloc(sizeof(char) * strlen(client->pwd));
     memset(new, 0, strlen(client->pwd));
     for (int i = strlen(client->pwd) - 1; i >= 0; i--) {
